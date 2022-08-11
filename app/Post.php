4f42e5aca422c37fc3180f7d031da12de1df9535
naomal <asikan.010@gmail.com>
2022-08-11 15:02:25 +0900

    add pagination

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
     public function getByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
