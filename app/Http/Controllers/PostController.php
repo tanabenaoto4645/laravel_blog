89456769166b3132edb73fbe9045504ca41161ba
naomal <asikan.010@gmail.com>
2022-08-13 09:33:44 +0900

    add newPost page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,4 +19,8 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
+    public function create(Post $post)
+    {
+        return view('posts/create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@
 */
 
 Route::get('/posts', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
 
